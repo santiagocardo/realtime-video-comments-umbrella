3117370cdd12237bf94eb46d2c7bdfbfa1afded0
Santiago Cardona <santiagocardo80@gmail.com>
2021-02-23 14:21:53 -0500

    ☂️  First commit: Rumbl as umbrella app

---
+++ apps/rumbl_web/lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,29 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+  alias Rumbl.Accounts
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => password}}) do
+    case Accounts.authenticate_by_username_and_pass(username, password) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> RumblWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
